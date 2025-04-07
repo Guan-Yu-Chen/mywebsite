6181803c78130327b7bacc3f2e82f73b2cf4b335
Guan-Yu-Chen <155933115+Guan-Yu-Chen@users.noreply.github.com>
2025-04-07 15:21:29 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,13 @@
-alert()
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("showTimeButton");
+  button.addEventListener("click", function () {
+    displayCurrentTime();
+  });
+
+  function displayCurrentTime() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString();
+    document.getElementById("timeDisplay").innerText =
+      "現在時間: " + timeString;
+  }
+});
